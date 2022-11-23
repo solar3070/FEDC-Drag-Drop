ff515dafa2ed05943c4f2565ed75d6ef036e2974
HyeJun <solar3070@naver.com>
2022-11-23 14:26:47 +0900

    feat: 드래그 앤 드롭 구현

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 const API_END_POINT = "https://kdt-frontend.todo-api.programmers.co.kr/roto";
 
-export const request = async (url) => {
+export const request = async (url, options) => {
   try {
-    const res = await fetch(`${API_END_POINT}${url}`);
+    const res = await fetch(`${API_END_POINT}${url}`, options);
 
     if (!res.ok) {
       throw new Error("API 호출 오류");
